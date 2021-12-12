06231eab19f0e08b1857c3b3a9d417e8c84390c1
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-12 09:54:54 +0700

    fix: link route tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-  
 </script>
 
-<main>
-  
+<main class="bg-gray-100">
+    <div class="mx-auto max-w-4xl py-5 px-3">
+        <h1>Naruto API Client</h1>
+    </div>
 </main>
 
 <style>
-  
 </style>
